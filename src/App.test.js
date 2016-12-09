333a82c03fd279decc779d52890193168b0e2b10
Sean Smith <sean.smith.2009@gmail.com>
2016-12-08 16:05:57 -0800

    added a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import expect from 'expect';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(React.createElement(App), div);
 });
+
+const shallowRender = shallow(React.createElement(App));
+
+it('App returns a div', () => {
+	expect(shallowRender.find('div').length).toEqual(1);
+});
+
