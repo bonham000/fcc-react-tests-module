7398a60e7dd248e728457eb1474202c1fc6b0ef3
Sean Smith <sean.smith.2009@gmail.com>
2017-12-01 20:18:01 -0800

    Replaces tabs with spaces in modified React 30

--- src/challenges/react/React_30.js
+++ src/challenges/react/React_30.js
@@ -31,36 +31,36 @@ Finally, create an <code>h1</code> tag after the <code>form</code> which renders
 // ---------------------------- define challenge seed code ----------------------------
 export const seedCode =
 `class MyForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			input: '',
-			submit: ''
-		};
-	}
-	handleChange = (event) => {
-		this.setState({
-			input: event.target.value
-		});
-	}
-	handleSubmit = (event) => {
-		// change code below this line
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit = (event) => {
+    // change code below this line
 
-		// change code above this line
-	}
-	render() {
-  	return (
-  		<div>
-		    <form onSubmit={this.handleSubmit}>
-	        { /* change code below this line */ }
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
 
-	        { /* change code above this line */ }
-	        <button type='submit'>Submit!</button>
-		    </form>
-		    { /* change code below this line */ }
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
 
-	      { /* change code above this line */ }
-		  </div>
+        { /* change code above this line */ }
+      </div>
     );
   }
 };`
@@ -68,35 +68,35 @@ export const seedCode =
 // ---------------------------- define challenge solution code ----------------------------
 export const solutionCode =
 `class MyForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			input: '',
-			submit: ''
-		};
-	}
-	handleChange = (event) => {
-		this.setState({
-			input: event.target.value
-		});
-	}
-	handleSubmit = (event) => {
-		event.preventDefault()
-		this.setState({
-			submit: this.state.input
-		});
-	}
-	render() {
-  	return (
-  		<div>
-		   	<form onSubmit={this.handleSubmit}>
-	        <input
-	        	value={this.state.input}
-	        	onChange={this.handleChange} />
-	        <button type='submit'>Submit!</button>
-		    </form>
-				<h1>{this.state.submit}</h1>
-		  </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+  render() {
+    return (
+      <div>
+         <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.input}
+            onChange={this.handleChange} />
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
     );
   }
 };`
@@ -105,155 +105,155 @@ export const solutionCode =
 
 export const executeTests = (code, errorSuppression) => {
 
-	const error_0 = 'Your JSX code should transpile successfully.';
-	const error_1 = 'MyForm should return a div element which contains a form and an h1 tag. The form should include an input and a button.';
-	const error_2 = 'The state of MyForm should initialize with input and submit properties, both set to empty strings.';
-	const error_3 = 'Typing in the input element should update the input property in the state.';
-	const error_4 = 'Submitting the form should run handleSubmit which should set the submit property in state equal to the current input.';
-	const error_5 = 'The h1 element should render the value of the submit field from the component\'s state.';
+  const error_0 = 'Your JSX code should transpile successfully.';
+  const error_1 = 'MyForm should return a div element which contains a form and an h1 tag. The form should include an input and a button.';
+  const error_2 = 'The state of MyForm should initialize with input and submit properties, both set to empty strings.';
+  const error_3 = 'Typing in the input element should update the input property in the state.';
+  const error_4 = 'Submitting the form should run handleSubmit which should set the submit property in state equal to the current input.';
+  const error_5 = 'The h1 element should render the value of the submit field from the component\'s state.';
 
-	let testResults = [
-		{
-			test: 0,
-			status: false,
-			condition: error_0
-		},
-		{
-			test: 1,
-			status: false,
-			condition: error_1
-		},
-		{
-			test: 2,
-			status: false,
-			condition: error_2
-		},
-		{
-			test: 3,
-			status: false,
-			condition: error_3
-		},
-		{
-			test: 4,
-			status: false,
-			condition: error_4
-		},
-		{
-			test: 5,
-			status: false,
-			condition: error_5
-		}
+  let testResults = [
+    {
+      test: 0,
+      status: false,
+      condition: error_0
+    },
+    {
+      test: 1,
+      status: false,
+      condition: error_1
+    },
+    {
+      test: 2,
+      status: false,
+      condition: error_2
+    },
+    {
+      test: 3,
+      status: false,
+      condition: error_3
+    },
+    {
+      test: 4,
+      status: false,
+      condition: error_4
+    },
+    {
+      test: 5,
+      status: false,
+      condition: error_5
+    }
 
-	];
+  ];
 
-	let es5, shallowRender, mockedComponent, passed = true;
+  let es5, shallowRender, mockedComponent, passed = true;
 
-	const exportScript = '\n export default MyForm'
-	const modifiedCode = code.concat(exportScript);
+  const exportScript = '\n export default MyForm'
+  const modifiedCode = code.concat(exportScript);
 
-	// test 0: try to transpile JSX, ES6 code to ES5 in browser
-	try {
-		es5 = transform(modifiedCode, { presets: [ 'es2015', 'stage-2', 'react' ] }).code;
-		testResults[0].status = true;
-		if (!errorSuppression) console.log('No transpilation errors!');
-	} catch (err) {
-		passed = false;
-		testResults[0].status = false;
-		if (!errorSuppression) console.error(`Transpilation error: ${err}`);
-	}
+  // test 0: try to transpile JSX, ES6 code to ES5 in browser
+  try {
+    es5 = transform(modifiedCode, { presets: [ 'es2015', 'stage-2', 'react' ] }).code;
+    testResults[0].status = true;
+    if (!errorSuppression) console.log('No transpilation errors!');
+  } catch (err) {
+    passed = false;
+    testResults[0].status = false;
+    if (!errorSuppression) console.error(`Transpilation error: ${err}`);
+  }
 
-	// try to shallow render the component with Enzyme
-	try {
-		mockedComponent = mount(React.createElement(eval(es5)));
-	} catch (err) {
-		passed = false;
-		if (!errorSuppression) console.error(`Invalid React code: ${err}`);
-	}
+  // try to shallow render the component with Enzyme
+  try {
+    mockedComponent = mount(React.createElement(eval(es5)));
+  } catch (err) {
+    passed = false;
+    if (!errorSuppression) console.error(`Invalid React code: ${err}`);
+  }
 
-	// test 1:
-	try {
-		assert(
-			mockedComponent.find('div').children().find('form').length === 1,
-			mockedComponent.find('div').children().find('h1').length === 1,
-			mockedComponent.find('form').children().find('input').length === 1 &&
-			mockedComponent.find('form').children().find('button').length === 1 &&
-			error_1
-		);
-		testResults[1].status = true;
-	} catch (err) {
-		passed = false;
-		testResults[1].status = false;
-	}
+  // test 1:
+  try {
+    assert(
+      mockedComponent.find('div').children().find('form').length === 1,
+      mockedComponent.find('div').children().find('h1').length === 1,
+      mockedComponent.find('form').children().find('input').length === 1 &&
+      mockedComponent.find('form').children().find('button').length === 1 &&
+      error_1
+    );
+    testResults[1].status = true;
+  } catch (err) {
+    passed = false;
+    testResults[1].status = false;
+  }
 
-	// test 2:
-	try {
-		assert(
-			mockedComponent.state('input') === '' &&
-			mockedComponent.state('submit') === '',
-			error_2
-		);
-		testResults[2].status = true;
-	} catch (err) {
-		passed = false;
-		testResults[2].status = false;
-	}
+  // test 2:
+  try {
+    assert(
+      mockedComponent.state('input') === '' &&
+      mockedComponent.state('submit') === '',
+      error_2
+    );
+    testResults[2].status = true;
+  } catch (err) {
+    passed = false;
+    testResults[2].status = false;
+  }
 
-	//test 3:
-	try {
+  //test 3:
+  try {
 
-		mockedComponent.setState({input: ''});
-		const before = mockedComponent.state('input');
-		mockedComponent.find('input').simulate('change', {target: {value: 'TestInput'}});
-		const after = mockedComponent.state('input');
-		const inputText = mockedComponent.find('input').node.value;
+    mockedComponent.setState({input: ''});
+    const before = mockedComponent.state('input');
+    mockedComponent.find('input').simulate('change', {target: {value: 'TestInput'}});
+    const after = mockedComponent.state('input');
+    const inputText = mockedComponent.find('input').node.value;
 
-		assert.strictEqual(before === '' && after === 'TestInput' && inputText === 'TestInput', true, error_3);
-		testResults[3].status = true;
-	} catch (err) {
-		passed = false;
-		testResults[3].status = false;
-	}
+    assert.strictEqual(before === '' && after === 'TestInput' && inputText === 'TestInput', true, error_3);
+    testResults[3].status = true;
+  } catch (err) {
+    passed = false;
+    testResults[3].status = false;
+  }
 
-	//test 4:
-	try {
+  //test 4:
+  try {
 
-		mockedComponent.setState({input: ''});
-		mockedComponent.setState({submit: ''});
-		mockedComponent.find('input').simulate('change', {target: {value: 'SubmitInput'}});
-		const submitBefore = mockedComponent.state('submit');
-		mockedComponent.find('form').simulate('submit');
-		const submitAfter = mockedComponent.state('submit');
+    mockedComponent.setState({input: ''});
+    mockedComponent.setState({submit: ''});
+    mockedComponent.find('input').simulate('change', {target: {value: 'SubmitInput'}});
+    const submitBefore = mockedComponent.state('submit');
+    mockedComponent.find('form').simulate('submit');
+    const submitAfter = mockedComponent.state('submit');
 
-		assert.strictEqual(submitBefore === '' && submitAfter === 'SubmitInput', true, error_4);
-		testResults[4].status = true;
-	} catch (err) {
-		passed = false;
-		testResults[4].status = false;
-	}
+    assert.strictEqual(submitBefore === '' && submitAfter === 'SubmitInput', true, error_4);
+    testResults[4].status = true;
+  } catch (err) {
+    passed = false;
+    testResults[4].status = false;
+  }
 
-	//test 5:
-	try {
+  //test 5:
+  try {
 
-		mockedComponent.setState({input: ''});
-		mockedComponent.setState({submit: ''});
+    mockedComponent.setState({input: ''});
+    mockedComponent.setState({submit: ''});
 
-		mockedComponent.find('input').simulate('change', {target: {value: 'TestInput'}});
-		const h1Before = mockedComponent.find('h1').node.innerText;
+    mockedComponent.find('input').simulate('change', {target: {value: 'TestInput'}});
+    const h1Before = mockedComponent.find('h1').node.innerText;
 
-		mockedComponent.find('form').simulate('submit');
-		const h1After = mockedComponent.find('h1').node.innerText;
+    mockedComponent.find('form').simulate('submit');
+    const h1After = mockedComponent.find('h1').node.innerText;
 
-		assert.strictEqual(h1Before === '' && h1After === 'TestInput', true, error_5);
-		testResults[5].status = true;
-	} catch (err) {
-		passed = false;
-		testResults[5].status = false;
-	}
+    assert.strictEqual(h1Before === '' && h1After === 'TestInput', true, error_5);
+    testResults[5].status = true;
+  } catch (err) {
+    passed = false;
+    testResults[5].status = false;
+  }
 
-	return {
-		passed,
-		testResults
-	}
+  return {
+    passed,
+    testResults
+  }
 
 }
 
@@ -261,14 +261,14 @@ export const executeTests = (code, errorSuppression) => {
 
 export const liveRender = (code) => {
 
-	try {
-		const exportScript = '\n export default MyForm'
-		const modifiedCode = code.concat(exportScript);
-		const es5 = transform(modifiedCode, { presets: [ 'es2015', 'stage-2', 'react' ] }).code;
-		const renderedComponent = React.createElement(eval(es5));
-		return renderedComponent;
-	} catch (err) {
-		// console.log(`Live rendering failure: ${err}`);
-	}
+  try {
+    const exportScript = '\n export default MyForm'
+    const modifiedCode = code.concat(exportScript);
+    const es5 = transform(modifiedCode, { presets: [ 'es2015', 'stage-2', 'react' ] }).code;
+    const renderedComponent = React.createElement(eval(es5));
+    return renderedComponent;
+  } catch (err) {
+    // console.log(`Live rendering failure: ${err}`);
+  }
 
 }
