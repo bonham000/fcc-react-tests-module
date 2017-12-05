061cba60d0d9c4baa67272b750736e0f36df8db5
Sean Smith <sean.smith.2009@gmail.com>
2017-12-04 21:36:13 -0800

    Adds code tag wrapper for test converter

--- conversion-utils/test-converter/converter.js
+++ conversion-utils/test-converter/converter.js
@@ -14,6 +14,25 @@ const updateCheckbox = (e) => {
 	label.innerHTML = `<code>${text}</code>`;
 }
 
+/* Wrap easily identifiable keywords in <code> tags for FCC */
+const wrapCodeTags = (string) => {
+	return string
+		.replace(/ p /g, ' <code>p</code> ')
+		.replace(/ br /g, ' <code>br</code> ')
+		.replace(/ hr /g, ' <code>hr</code> ')
+		.replace(/ h1 /g, ' <code>h1</code> ')
+		.replace(/ h2 /g, ' <code>h2</code> ')
+		.replace(/ h3 /g, ' <code>h3</code> ')
+		.replace(/ h4 /g, ' <code>h4</code> ')
+		.replace(/ h5 /g, ' <code>h5</code> ')
+		.replace(/ h6 /g, ' <code>h6</code> ')
+		.replace(/ ul /g, ' <code>ul</code> ')
+		.replace(/ li /g, ' <code>li</code> ')
+		.replace(/ div /g, ' <code>div</code> ')
+		.replace(/ span /g, ' <code>span</code> ')
+		.replace(/ propTypes /g, ' <code>propTypes</code> ');
+}
+
 // Clear confirmation message on change
 const handleChange = () => confirmation('');
 
@@ -39,7 +58,7 @@ const convert = () => {
 		.replace(/\n/g, ' ') // multi-line string to single-line
 		.replace(/\s\s+/g, ' ') // replace multiple spaces
 		.replace(/;;+/g, ';'); // replace multiple semicolons
-	const convertedMessage = `'message: ${message}'`;
+	const convertedMessage = `'message: ${wrapCodeTags(message)}'`;
 
 	const { checked } = document.getElementById('conversionTypeCheckbox');
 	const testLine = !checked
